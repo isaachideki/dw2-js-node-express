b117f397f93aa6af2b4392cc6496fb0439cb22ee
DSM2 <fatec-dsm2@academico.intra>
2025-08-25 18:42:45 -0300

    exercicios

---
+++ exercicios/exercicio1/js.js
@@ -0,0 +1,7 @@
+const name = "Isaac Hideki Shiokawa";
+
+function saudacaoPersonalizada(name) {
+  document.write(`<h2>Olá, ${name}! Bem-vindo(a) à Calculadora Universal!</h2>`);
+}
+
+saudacaoPersonalizada (name);
---
+++ exercicios/exercicio5/js5.js
@@ -0,0 +1,4 @@
+
+const iife = function(){
+    document.write("<p>Calculadora Universal pronta para uso!</p>");
+}();
