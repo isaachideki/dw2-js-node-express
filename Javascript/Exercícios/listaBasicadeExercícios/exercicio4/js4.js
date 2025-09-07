e64d262da59e629a23f36608af2476b957a1137b
isaachideki <hidekisaac1@gmail.com>
2025-09-07 18:53:41 -0300

    js

--- Javascript/Exercícios/listaBasicadeExercícios/exercicio4/js4.js
+++
@@ -1,5 +0,0 @@
-const valorMetade = prompt("Digite um número:");
-const calcularMetade = (n) => {
-  return n / 2;
-};
-document.write(`<p>A metade do número digitado é: <strong>${calcularMetade(valorMetade)}</strong></p>`);
